2551c0d472a73e1218d7b43e777f8f563e455e83
tejasvp25 <tejasvp25@gmail.com>
2022-12-01 10:30:03 +0530

    Update JSON formats
    Update Status codes acording to HTTP conventions
    
    misc
    
    test
    
    test1
    
    test2

--- main.py
+++ main.py
@@ -4,110 +4,162 @@ import MySQLdb.cursors
 import re
 import os
 from werkzeug.utils import secure_filename
-from utils import handleMisProd
-from utils import deleteConvertedXLS
-from utils import handleDailyProd
+from utils import handleMisProd, handleDailyProd, deleteConvertedXLS, sendForgotPasswordMail
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from uuid import uuid4
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+CORS(app)
 
 app.secret_key = "fluid_mech"
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "Fluid Logins"
+app.config["MYSQL_PASSWORD"] = "sarthak333"
+app.config["MYSQL_DB"] = "fluid_control"
 
 mysql = MySQL(app)
 
 
 @app.route('/')
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["POST"])
 def login():
     if request.method == 'POST':
         username = request.json.get("username")
         password = request.json.get("password")
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
-            "SELECT * FROM accounts WHERE username = % s AND password = % s", (username, password, ))
+            "SELECT * FROM accounts WHERE username=%s", (username,))
         account = cursor.fetchone()
-
-        if account:
-            session["loggedin"] = True
-            session["id"] = account["id"]
-            session["username"] = account["username"]
+        # print(account)
+        # account = None
+        # print(str())
+        if account and bcrypt.check_password_hash(account['password'], password):
+            # session["loggedin"] = True
+            # session["id"] = account["id"]
+            # session["username"] = account["username"]
             return jsonify({
-                "status": 200,
                 "message": "Loggen in Successfully"
-            })
+            }), 200
         else:
             return jsonify({
-                "status": 0,
                 "message": "Incorrect username or password"
-            })
+            }), 400
     else:
         return jsonify({
-            "status": 0,
             "message": "Server Error!"
-        })
+        }), 401
 
 
 @app.route("/logout")
 def logout():
-    session.pop("loggedin", None)
-    session.pop("id", None)
-    session.pop("username", None)
+    # session.pop("loggedin", None)
+    # session.pop("id", None)
+    # session.pop("username", None)
     return jsonify({
-        "status": 200,
         "message": "Logged Out!"
-    })
+    }), 200
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/resetpassword', methods=['POST'])
+def resetPassword():
+    uniqueID = request.json.get("uniqueid")
+    newPassword = request.json.get("password")
+    if uniqueID == None:
+        return jsonify({"msg": "Please Provide UniqueID"}), 400
+    elif newPassword == None:
+        return jsonify({"msg": "Please Provide New Password"}), 400
+    else:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            "SELECT a.* FROM accounts a INNER JOIN forgotpassword fp ON a.id = fp.userid WHERE fp.uniqueid =%s", (uniqueID,))
+        account = cursor.fetchone()
+        if account:
+            newPassword = bcrypt.generate_password_hash(
+                newPassword).decode("utf-8")
+            cursor.execute(
+                "DELETE FROM forgotpassword WHERE uniqueid =%s", (uniqueID,))
+            print(int(account['id']))
+            cursor.execute(
+                "UPDATE accounts SET password=%s WHERE id=%s", (newPassword, int(account['id']),))
+            mysql.connection.commit()
+            return jsonify({}), 200
+        else:
+            return jsonify({}), 400
+
+
+@ app.route('/forgotpassword', methods=['POST'])
+def forgotPassword():
+    email = request.json.get("email")
+    if (email == None):
+        return jsonify({"msg": "Please Provide an valid Email"}), 400
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(
+        "SELECT * FROM accounts WHERE email=%s", (email,))
+    account = cursor.fetchone()
+    if account:
+        uniqueID = uuid4()
+        cursor.execute(
+            'INSERT INTO forgotpassword VALUES (NULL, %s, %s )', (account['id'], uniqueID,))
+        mysql.connection.commit()
+        try:
+            sendForgotPasswordMail(
+                account['email'], account['username'], uniqueID)
+            return jsonify({"msg": "Password Reset Link is Sent Successfully, Please Check your Email"}), 200
+        except:
+            return jsonify({}), 500
+    else:
+        return jsonify({"msg": "Please Provide an valid Email"}), 400
+
+
+@ app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
     if request.method == 'POST':
         username = request.json.get('username')
         password = request.json.get('password')
         email = request.json.get('email')
+        firstname = request.json.get('firstname')
+        lastname = request.json.get('lastname')
+        date_of_birth = request.json.get('dob')
+
+        password = bcrypt.generate_password_hash(password).decode("utf-8")
+
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(
             'SELECT * FROM accounts WHERE username = % s', (username, ))
         account = cursor.fetchone()
         if account:
             return jsonify({
-                "status": 0,
                 "message": 'Account already exists !',
-            })
+            }), 400
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             return jsonify({
-                "status": 0,
                 "message": 'Invalid email address !',
-            })
+            }), 400
         elif not re.match(r'[A-Za-z0-9]+', username):
             return jsonify({
-                "status": 0,
                 "message": 'Username must contain only characters and numbers !',
-            })
+            }), 400
         elif not username or not password or not email:
             return jsonify({
-                "status": 0,
                 "message": 'Please fill out the form !',
-            })
+            }), 400
         else:
             cursor.execute(
-                'INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+                'INSERT INTO accounts VALUES (NULL, %s, %s, %s,%s,%s,%s)', (firstname, lastname, username, password, email, date_of_birth))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
             return jsonify({
-                "status": 200,
                 "message": msg,
-            })
+            }), 200
     else:
         msg = 'Please fill out the form !'
         return jsonify({
-            "status": 0,
             "message": msg,
-        })
+        }), 401
 
 
 def allowedFiles(fileName):
@@ -122,85 +174,59 @@ def allowedFiles(fileName):
         return False
 
 
-@app.route("/dailyProd", methods=["POST"])
+@ app.route("/dailyprod", methods=["POST"])
 def dailyProdHandler():
     if request.method == "POST":
         if request.files:
             f = request.files["file"]
 
             if f.filename == "":
-                return jsonify({
-                    "status": 0,
-                    "message": "No file selected",
-                })
+                return jsonify({}), 401
+
+            if f.filename.endswith("xlsx"):
+                return jsonify({}), 500
 
             if not allowedFiles(f.filename):
-                return jsonify({
-                    "status": 0,
-                    "message": "only excel files are allowed",
-                })
+                return jsonify({}), 401
 
             else:
-                fileName = secure_filename(f.filename)
-                f.save(fileName)
-                res = handleDailyProd(fileName)
-                deleteConvertedXLS(fileName)
-                return jsonify({
-                    "status": 200,
-                    "message": "Data converted successfully!",
-                    "response": res,
-                    "mimetype": "application/json"
-                })
+                try:
+                    fileName = secure_filename(f.filename)
+                    f.save(fileName)
+                    res = handleDailyProd(fileName)
+                    deleteConvertedXLS(fileName)
+                    return jsonify(res)
+                except Exception:
+                    print(Exception)
+                    return jsonify({}), 500
         else:
-            return jsonify({
-                "status": 0,
-                "message": "Failed to upload file!"
-            })
+            return jsonify({}), 401,
     else:
-        return jsonify({
-            "status": 500,
-            "message": "Server Error!"
-        })
+        return jsonify({}), 500
 
-@app.route("/misprod", methods=["POST"])
+
+@ app.route("/misprod", methods=["POST"])
 def misProdHandler():
     if request.method == "POST":
         if request.files:
             f = request.files["file"]
 
             if f.filename == "":
-                return jsonify({
-                    "status": 0,
-                    "message": "No file selected",
-                })
+                return jsonify({}), 401
 
             if not allowedFiles(f.filename):
-                return jsonify({
-                    "status": 0,
-                    "message": "only excel files are allowed",
-                })
+                return jsonify({}), 401
 
             else:
                 fileName = secure_filename(f.filename)
                 f.save(fileName)
                 res = handleMisProd(fileName)
                 deleteConvertedXLS(fileName)
-                return jsonify({
-                    "status": 200,
-                    "message": "Data converted successfully!",
-                    "response": res,
-                    "mimetype": "application/json"
-                })
+                return jsonify(res)
         else:
-            return jsonify({
-                "status": 0,
-                "message": "Failed to upload file!"
-            })
+            return jsonify({}), 401
     else:
-        return jsonify({
-            "status": 500,
-            "message": "Server Error!"
-        })
+        return jsonify({}), 500
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -4,6 +4,12 @@ from openpyxl import load_workbook
 import xlrd
 import json
 import os
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+frontEndURL = "http://localhost:3000"
 
 
 def convertToXLSX(orgFileName, newFileName):
@@ -97,33 +103,37 @@ def generateJsonForDailyProd(workbook):
             row['partname'] = key
             rows.append(row)
 
-        majors = {}
+        majors = []
 
-        majors["max_lossqty"] = df[["partname", "loss_qty"]].sort_values(
-            by="loss_qty", ascending=False).head(5).to_dict(orient="records")
+        majors.append({"max_lossqty": df[["partname", "loss_qty"]].sort_values(
+            by="loss_qty", ascending=False).head(5).to_dict(orient="records")})
 
-        majors["max_actual_prod_time"] = df[["partname", "actual_prod_time"]].sort_values(
+        majors.append({"max_actual_prod_time": df[["partname", "actual_prod_time"]].sort_values(
             by="actual_prod_time", ascending=False).head(5).to_dict(orient="records")
+        })
+        majors.append({"max_prod_per_hr": df[["partname", "prod_per_hr"]].sort_values(
+            by="prod_per_hr", ascending=False).head(5).to_dict(orient="records")})
 
-        majors["max_prod_per_hr"] = df[["partname", "prod_per_hr"]].sort_values(
-            by="prod_per_hr", ascending=False).head(5).to_dict(orient="records")
+        majors.append({"least_prod_per_hr": df[["partname", "prod_per_hr"]].sort_values(
+            by="prod_per_hr", ascending=True).head(5).to_dict(orient="records")})
 
-        majors["least_prod_per_hr"] = df[["partname", "prod_per_hr"]].sort_values(
-            by="prod_per_hr", ascending=True).head(5).to_dict(orient="records")
+        majors.append({"max_loss_mc_hr": df[["partname", "loss_mc_hr"]].sort_values(
+            by="loss_mc_hr", ascending=False).head(5).to_dict(orient="records")})
 
-        majors["max_loss_mc_hr"] = df[["partname", "loss_mc_hr"]].sort_values(
-            by="loss_mc_hr", ascending=False).head(5).to_dict(orient="records")
+        majors.append({"least_loss_mc_hr": df[["partname", "loss_mc_hr"]].sort_values(
+            by="loss_mc_hr", ascending=True).head(5).to_dict(orient="records")})
 
-        majors["least_loss_mc_hr"] = df[["partname", "loss_mc_hr"]].sort_values(
-            by="loss_mc_hr", ascending=True).head(5).to_dict(orient="records")
+        majors.append({"max_qty_req": df[["partname", "qty_req"]].sort_values(
+            by="qty_req", ascending=False).head(5).to_dict(orient="records")})
 
-        majors["max_qty_req"] = df[["partname", "qty_req"]].sort_values(
-            by="qty_req", ascending=False).head(5).to_dict(orient="records")
+        majors.append({"max_qty_achieved": df[["partname", "qty_achieved"]].sort_values(
+            by="qty_achieved", ascending=False).head(5).to_dict(orient="records")})
 
-        majors["max_qty_achieved"] = df[["partname", "qty_achieved"]].sort_values(
-            by="qty_achieved", ascending=False).head(5).to_dict(orient="records")
+        majorTitles = ["Top 5 Parts with Maximum Loss",
+                       "Top 5 Parts with Actual Production", "Top 5 Parts with Maximum prod per hour", "Top 5 parts with Least Production Per Hour", "Top 5 parts with Maximum MC hours loss", "Top 5 parts with Least MC hours loss", "Top 5 Parts with Maximum Quantity Required", "Top 5 Parts with Maximum Quantity Achieved"]
 
-        res[ws.title].update({"rows": rows, "majors": majors})
+        res[ws.title].update(
+            {"rows": rows, "majors": majors, "majorTitles": majorTitles})
 
         # print(res)
         # res = res
@@ -148,54 +158,58 @@ def handleMisProd(filename):
 def handleMisProdMachineShop(ws):
     dic = {}
     mainH = ws.cell(10, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {}
+        dateDic = {}
         for col in range(11, 16):
             key = ws.cell(col, 2).value.strip()
-            dic[mainH][datee][key] = []
+            dateDic[key] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][key].append(
+                dateDic[key].append(
                     valToAppend if valToAppend != None else 0)
+        dic[mainH].append(dateDic)
 
     mainH = ws.cell(17, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
+    dic["months"] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {}
+        dic["months"].append(datee)
+        dateDic = {}
         for col in range(18, 23):
             key = ws.cell(col, 2).value.strip()
-            dic[mainH][datee][key] = []
+            dateDic[key] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][key].append(
+                dateDic[key].append(
                     valToAppend if valToAppend != None else 0)
+        dic[mainH].append(dateDic)
 
-    lol = []
-    for key, value in dic.items():
-        # temp = dict()
-        # temp['detail'] = key
-        for key1, value1 in value.items():
-            # temp['month'] = key1
-            for key2, value2 in value1.items():
-                # print(value2[0])
-                # temp['subdetail'] = key2
-                # temp['fitting'] = value2[0]
-                # temp['valves'] = value2[1]
-                # temp['clamp'] = value2[2]
-                lol.append({'detail': key, 'month': key1,
-                            'subdetail': key2, 'fitting': value2[0], 'valves': value2[1], 'clamp': value2[2]})
-        temp = dict()
+    # lol = []
+    # for key, value in dic.items():
+    #     # temp = dict()
+    #     # temp['detail'] = key
+    #     for key1, value1 in value.items():
+    #         # temp['month'] = key1
+    #         for key2, value2 in value1.items():
+    #             # print(value2[0])
+    #             # temp['subdetail'] = key2
+    #             # temp['fitting'] = value2[0]
+    #             # temp['valves'] = value2[1]
+    #             # temp['clamp'] = value2[2]
+    #             lol.append({'detail': key, 'month': key1,
+    #                         'subdetail': key2, 'fitting': value2[0], 'valves': value2[1], 'clamp': value2[2]})
+    #     temp = dict()
 
     # print(pd.DataFrame.from_records(lol).to_json(orient="records"))
     return dic
@@ -204,100 +218,90 @@ def handleMisProdMachineShop(ws):
 def handleMisProdInHouseProd(ws):
     dic = {}
     mainH = ws.cell(8, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {"total": []}
-        total = [0, 0, 0, 0]
+        dateDic = {}
 
         for col in range(9, 14):
-            dic[mainH][datee][ws.cell(col, 2).value] = []
+            dateDic[ws.cell(col, 2).value] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][ws.cell(col, 2).value].append(
+                dateDic[ws.cell(col, 2).value].append(
                     valToAppend if valToAppend != None else 0)
-                total[k] = total[k] + \
-                    (valToAppend if valToAppend != None else 0)
-        dic[mainH][datee]["total"] = total
+        dic[mainH].append(dateDic)
 
     mainH = ws.cell(15, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {"total": []}
-        total = [0, 0, 0, 0]
+        dateDic = {}
         for col in range(17, 20):
-            dic[mainH][datee][ws.cell(col, 2).value] = []
+            dateDic[ws.cell(col, 2).value] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][ws.cell(col, 2).value].append(
+                dateDic[ws.cell(col, 2).value].append(
                     valToAppend if valToAppend != None else 0)
-                total[k] = total[k] + \
-                    (valToAppend if valToAppend != None else 0)
-        dic[mainH][datee]["total"] = total
+        dic[mainH].append(dateDic)
 
     mainH = ws.cell(21, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {"total": []}
-        total = [0, 0, 0, 0]
+        dateDic = {}
         for col in range(23, 29):
-            dic[mainH][datee][ws.cell(col, 2).value] = []
+            dateDic[ws.cell(col, 2).value] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][ws.cell(col, 2).value].append(
+                dateDic[ws.cell(col, 2).value].append(
                     valToAppend if valToAppend != None else 0)
-                total[k] = total[k] + \
-                    (valToAppend if valToAppend != None else 0)
-        dic[mainH][datee]["total"] = total
+        dic[mainH].append(dateDic)
 
     mainH = ws.cell(30, 2).value
-    dic[mainH] = {}
+    dic[mainH] = []
+    dic["months"] = []
     # row = Row , col = Col
     for row in range(6, 1000, 5):
         datee = ws.cell(4, row)
         if (datee.value == None):
             break
         datee = datee.value.strftime("%b-%Y")
-        dic[mainH][datee] = {"total": []}
-        total = [0, 0, 0, 0]
+        dic["months"].append(datee)
+        dateDic = {}
         for col in range(33, 36):
-            dic[mainH][datee][ws.cell(col, 3).value] = []
+            dateDic[ws.cell(col, 3).value] = []
             for k in range(0, 4):
                 valToAppend = ws.cell(col, row+k).value
-                dic[mainH][datee][ws.cell(col, 3).value].append(
+                dateDic[ws.cell(col, 3).value].append(
                     valToAppend if valToAppend != None else 0)
-                total[k] = total[k] + \
-                    (valToAppend if valToAppend != None else 0)
-        dic[mainH][datee]["total"] = total
+        dic[mainH].append(dateDic)
 
-    lol = []
-    for key, value in dic.items():
-        # temp = dict()
-        # temp['detail'] = key
-        for key1, value1 in value.items():
-            # temp['month'] = key1
-            for key2, value2 in value1.items():
-                # print(value2[0])
-                # temp['subdetail'] = key2
-                # temp['fitting'] = value2[0]
-                # temp['valves'] = value2[1]
-                # temp['clamp'] = value2[2]
-                lol.append({'detail': key, 'month': key1,
-                            'subdetail': key2, 'fitting': value2[0], 'valves': value2[1], 'clamp': value2[2]})
+    # lol = []
+    # for key, value in dic.items():
+    #     # temp = dict()
+    #     # temp['detail'] = key
+    #     for key1, value1 in value.items():
+    #         # temp['month'] = key1
+    #         for key2, value2 in value1.items():
+    #             # print(value2[0])
+    #             # temp['subdetail'] = key2
+    #             # temp['fitting'] = value2[0]
+    #             # temp['valves'] = value2[1]
+    #             # temp['clamp'] = value2[2]
+    #             lol.append({'detail': key, 'month': key1,
+    #                         'subdetail': key2, 'fitting': value2[0], 'valves': value2[1], 'clamp': value2[2]})
     return dic
 
 
@@ -308,3 +312,61 @@ def deleteConvertedXLS(filename):
     if (os.path.exists(filename)):
         os.remove(filename)
     pass
+
+
+def sendForgotPasswordMail(receiver_email, name, uniqueID):
+    port = 465  # For SSL
+    password = ""
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    sender_email = "tejasvp252@gmail.com"
+    # receiver_email = "tejasvp25@gmail.com"
+    # name = "Tejas"
+    link = "{}/resetpassword/{}".format(frontEndURL, uniqueID)
+
+    mailHTML = """\
+        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+    <tr>
+        <td style="height:40px;">&nbsp;</td>
+    </tr>
+    <tr>
+        <td style="padding:0 35px;">
+            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Hello {}</h1>
+            <br>
+            <h2 style="color:#1e1e2d; font-weight:500; margin:0;font-family:'Rubik',sans-serif;">You have requested to reset your password</h2>
+            <span
+                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;font-family:'Rubik',sans-serif;">A unique link to reset your
+                password has been generated for you. To reset your password, click the
+                following button and follow the instructions.
+            </p>
+            <a href={}
+                style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;font-family:'Rubik',sans-serif;">Reset
+                Password</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="height:40px;">&nbsp;</td>
+    </tr>
+    </table>""".format(name, link)
+
+    message = MIMEMultipart()
+    message["Subject"] = "Forgot Password"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message.attach(MIMEText(mailHTML, "html"))
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
+    try:
+        server.login("tejasvp252@gmail.com", password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    except Exception:
+        print(Exception)
+        # raise Exception
+        return False
+    finally:
+        server.quit()
+    return True
